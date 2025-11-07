7fe91ddf96b47bef4d4fb5e32eb3019b89ee85a3
CyberManACC <deon.coles@g.austincc.edu>
2025-11-07 02:49:25 +0000

    HW07: lists & tuples + tests

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,26 @@
-#
+def get_lowest_list_value(values):
+    if values is None or len(values) == 0:
+        raise ValueError("values must be a non-empty list")
+
+    lowest = values[0]
+    i = 1
+    n = len(values)
+    while i < n:
+        if values[i] < lowest:
+            lowest = values[i]
+        i += 1
+    return lowest
+
+
+def get_highest_list_value(values):
+    if values is None or len(values) == 0:
+        raise ValueError("values must be a non-empty list")
+
+    highest = values[0]
+    i = 1
+    n = len(values)
+    while i < n:
+        if values[i] > highest:
+            highest = values[i]
+        i += 1
+    return highest
